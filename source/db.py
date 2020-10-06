b9d4ccc54c529b5b56ec9e4dd6901c93e59411ff
Tommy-Tang-NYU <mt4145@nyu.edu>
2020-10-05 23:26:59 -0400

    Added db apis

--- source/db.py
+++ source/db.py
@@ -4,12 +4,31 @@ At first, it will just contain stubs that return dummy data.
 Gradually, we will fill in actual calls to our datastore.
 """
 
+all_housings = {1: ["33 Bond Street", "www.housing.com"],
+                3: ["474 Warrent Street", "www.housing.com"],
+                2: ["Avalon Ave", "www.housing.com"],
+                4: ["485 Clermont Ave", "www.housing.com"],
+                5: ["110 Sex Street", "www.housing.com"],
+                6: ["239 Hell Ave", "www.housing.com"]}
 
-def fetch_housing():
+
+def get_all_housing():
     """
     A function to return all housings in the data store.
     """
-    return {"33 Bond Street": 2,
-            "474 Warrent Street": 3,
-            "1 Avalon Avet": 1,
-            "485 Clermont Ave": 4}
+    return all_housings
+
+
+def get_housing_info(id):
+    """
+    A function to return a detailed housing with specified ID
+    """
+    return None if all_housings.get(id) is None else all_housings.get(id)
+
+
+def get_housing_info_link(id):
+    """
+    a function to return the web link for a housing with specified ID
+    """
+    return None if all_housings.get(id)is None or \
+        len(all_housings.get(id)) < 2 else all_housings.get(id)[1]
