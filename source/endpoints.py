a297796e3c2e2352c72c56785cef30c8db51ceb2
Tommy-Tang-NYU <mt4145@nyu.edu>
2020-10-19 16:46:41 -0400

    Fixed Linter errors; Switched to MongoDB instead of local db

--- source/endpoints.py
+++ source/endpoints.py
@@ -3,16 +3,133 @@ This is the file containing all of the endpoints for our flask web app - iHomie
 The endpoint called `endpoints` will return all available endpoints
 """
 
-from flask import Flask
+from flask import Flask, make_response, request, jsonify
 from flask_restx import Resource, Api, reqparse
-from db import get_all_housing, get_housing_info, get_housing_info_link, get_user_info, login, add_housing_info,\
-    delete_housing_info, update_housing_info, signup
+from flask_mongoengine import MongoEngine
+from api_config import DB_URI
+from db import get_user_info, login, signup
 
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('username')
 parser.add_argument('password')
+
+app.config["MONGODB_HOST"] = DB_URI
+
+db = MongoEngine()
+db.init_app(app)
+
+
+class Housing(db.Document):
+    """
+    This class defines the database for a generic housing type
+    """
+    housing_id = db.IntField()
+    name = db.StringField()
+    address = db.StringField()
+
+    def to_json(self):
+        return {
+            "housing_id": self.housing_id,
+            "name": self.name,
+            "address": self.address
+        }
+
+
+class User(db.Document):
+    """
+    This class defines the database for a generic user type for login/signup
+    """
+    user_id = db.IntField()
+    user_pwd = db.StringField()
+
+    def to_json(self):
+        return {
+            "user_id": self.user_id,
+            "user_pwd": self.user_pwd
+        }
+
+
+@api.route('/db_populate')
+class HousingDB(Resource):
+    """
+    This class will populate a demo initial housings database for housings
+    """
+    def post(self):
+        """
+        populate the initial database, returns 201 on success
+        """
+        housing1 = Housing(housing_id=4,
+                           name="867 Aagon Alley",
+                           address="867 Aagon Ave")
+        housing2 = Housing(housing_id=5,
+                           name="438 Gu Yillage",
+                           address="438 Dumb Street")
+        housing1.save()
+        housing2.save()
+        return make_response("", 201)
+
+
+@api.route('/housings')
+class AllHousings(Resource):
+    """
+    This class will serve as live, fetchable documentation of what endpoints
+    are available in the system
+    """
+    def get(self):
+        """
+        The `get()` method will return all housing information
+        """
+        housings = []
+        for housing in Housing.objects:
+            housings.append(housing)
+        return make_response(jsonify(housings), 200)
+
+    def post(self):
+        """
+        The `post()` method will create new housing detail
+        """
+        content = request.json
+        housing = Housing(housing_id=content['housing_id'],
+                          name=content['name'],
+                          address=content['address'])
+        housing.save()
+        return make_response("", 201)
+
+
+@api.route('/housings/<housing_id>', methods=['GET', 'PUT', 'DELETE'])
+class HousingItem(Resource):
+    """
+    This class serves to get, put, and delete housing item
+    """
+    def get(self, housing_id):
+        """
+        GET/  return housing details of housing with _id
+        """
+        housing_obj = Housing.objects(housing_id=housing_id).first()
+        if housing_obj:
+            return make_response(jsonify(housing_obj.to_json()), 200)
+        else:
+            return make_response("", 404)
+
+    def put(self, housing_id):
+        """
+        PUT/ update housing details of housing with _id, 204 on success
+        """
+        content = request.json
+        housing_obj = Housing.objects(housing_id=housing_id).first()
+        housing_obj.update(name=content['name'], address=content['address'])
+        return make_response("", 204)
+
+    def delete(self, housing_id):
+        """
+        DELETE/ delete housing details of housing with _id
+        """
+        housing_obj = Housing.objects(housing_id=housing_id).first()
+        housing_obj.delete()
+        return make_response("", 204)
+
 
 @api.route('/hello')
 class HelloWorld(Resource):
@@ -21,6 +138,35 @@ class HelloWorld(Resource):
         First endpoint to bridge running server
         """
         return {'hello': 'world'}
+
+
+@api.route('/login')
+class Login(Resource):
+    """
+    This class supports fetching a list of all housings
+    """
+    def get(self):
+        """
+        this method used for login
+        """
+        args = parser.parse_args()
+        username = args['username']
+        password = args['password']
+        return get_user_info(username) if login(username, password) else None
+
+
+@api.route('/signup')
+class Signup(Resource):
+    """
+    This class supports fetching a list of all housings
+    """
+    def put(self):
+        """
+        this method adds new user information
+        """
+        args = parser.parse_args()
+        signup(args['username'], args['password'])
+        return ''
 
 
 @api.route('/endpoints')
@@ -36,100 +182,6 @@ class Endpoints(Resource):
         """
         return {'end': 'point'}
 
-
-@api.route('/housings')
-class Housings(Resource):
-    """
-    This class supports fetching a list of all housings
-    """
-    def get(self):
-        """
-        this method returns all housings
-        """
-        return get_all_housing()
-
-
-@api.route('/housings/<int:id>')
-@api.response(404, 'Housing not found.')
-class HousingItem(Resource):
-    """
-    This class serves to get, put, and delete housing item
-    """
-    def get(self, id):
-        """
-        Returns details of a housing.
-        """
-        return get_housing_info(id)
-
-    def get_weblink(self, id):
-        """
-        Returns weblink of a housing.
-        """
-        return get_housing_info_link(id)
-
-@api.route('/login')
-@api.response(404, 'User not found.')
-class Login(Resource):
-    """
-    This class supports fetching a list of all housings
-    """
-    def get(self):
-        """
-        this method used for login
-        """
-        args = parser.parse_args()
-        username = args['username']
-        password = args['password']
-        return get_user_info(username) if login(username, password) else None
-
-@api.route('/signup')
-class Signup(Resource):
-    """
-    This class supports fetching a list of all housings
-    """
-    def put(self):
-        """
-        this method adds new user information
-        """
-        args = parser.parse_args()
-        signup(args['username'], args['password'])
-        return ''
-
-@api.route('/add/<string:address>+<string:link>')
-class AddHouseInfo(Resource):
-    """
-    This class supports fetching a list of all housings
-    """
-    def post(self, address, link):
-        """
-        this method adds housing information
-        """
-        add_housing_info(address, link)
-        return ''
-
-@api.route('/update/<int:id>+<string:address>')
-class UpdateHouseInfo(Resource):
-    """
-    This class supports fetching a list of all housings
-    """
-    def post(self, id, address):
-        """
-        this method updates housing address
-        """
-        update_housing_info(id, address)
-        return ''
-
-@api.route('/delete/<int:id>')
-class DeleteHouseInfo(Resource):
-    """
-    This class supports fetching a list of all housings
-    """
-    def delete(self, id):
-        """
-        this method deletes housing link
-        """
-        delete_housing_info(id)
-        return ''
 
 if __name__ == '__main__':
     app.run(debug=True)
