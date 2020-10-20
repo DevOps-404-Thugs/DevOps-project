893ca44a37eb290a134cd02ed7e02ed328c9a3f1
Tommy-Tang-NYU <mt4145@nyu.edu>
2020-10-19 23:00:52 -0400

    fixed syntax error

---
+++ source/api_config.py
@@ -0,0 +1,3 @@
+database_name = "API"
+mongodb_password = "tommy1220"
+DB_URI = "mongodb+srv://nyu:{}@tommy-ehomie-cluster-0.f2vln.mongodb.net/{}?retryWrites=true&w=majority".format(mongodb_password, database_name)
