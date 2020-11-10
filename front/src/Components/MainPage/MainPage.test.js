dcbd56dec1482ae4c05072d2926d2b2b0c00ef96
YGGGY <yvonneguocn@163.com>
2020-11-10 00:10:23 -0500

    Updates

--- front/src/Components/MainPage/MainPage.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import enableHooks from 'jest-react-hook-shallow';
-import MainPage from './MainPage';
-
-TextDecoderStream('test Main Page', () =>{
-    const component = shallow(<MainPage />);
-    expect(component.text()).toContain('house');
-});
