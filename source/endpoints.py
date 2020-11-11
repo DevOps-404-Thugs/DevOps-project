a4eb269bd8ad67ab76d7d603f301284b808c05f3
tommy1220 <tommy.tang.1220@gmail.com>
2020-11-10 22:49:20 -0500

    dummy change

--- source/endpoints.py
+++ source/endpoints.py
@@ -410,5 +410,5 @@ def get_current_user_id():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
