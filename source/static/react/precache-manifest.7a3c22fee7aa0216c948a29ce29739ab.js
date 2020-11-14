3505c6ad65b24524b8940f0e7ecebd091062525f
tommy1220 <tommy.tang.1220@gmail.com>
2020-11-14 16:06:21 -0500

    temp commit from wanrong

---
+++ source/static/react/precache-manifest.7a3c22fee7aa0216c948a29ce29739ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a15dc6149cb30b67323096ed92b1b9",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "2d3428bf51e1a015c8a6",
+    "url": "/static/react/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "86e4d1d029c6f4271ad5",
+    "url": "/static/react/static/css/main.514bb702.chunk.css"
+  },
+  {
+    "revision": "2d3428bf51e1a015c8a6",
+    "url": "/static/react/static/js/2.d2457393.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/react/static/js/2.d2457393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e4d1d029c6f4271ad5",
+    "url": "/static/react/static/js/main.74d85e39.chunk.js"
+  },
+  {
+    "revision": "8e296fbd909a2b159a80",
+    "url": "/static/react/static/js/runtime-main.10d973be.js"
+  }
+]);
