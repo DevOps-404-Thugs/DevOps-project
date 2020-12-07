2b18349de848af1eced70d1dfc2e526e63437e04
Wanrong Zhu <wanrongrosa@gmail.com>
2020-12-06 21:23:04 -0500

    Display random pictures for house

---
+++ source/static/react/precache-manifest.f3160a197f294a8c3399c7498584fbae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27150eb32d348a0b454b4e3de053ff2",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "298d6d1483cf50948015",
+    "url": "/static/react/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "ec102e87cff6690c3d59",
+    "url": "/static/react/static/css/main.9c603076.chunk.css"
+  },
+  {
+    "revision": "298d6d1483cf50948015",
+    "url": "/static/react/static/js/2.e5839739.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/react/static/js/2.e5839739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec102e87cff6690c3d59",
+    "url": "/static/react/static/js/main.638d5011.chunk.js"
+  },
+  {
+    "revision": "8e296fbd909a2b159a80",
+    "url": "/static/react/static/js/runtime-main.10d973be.js"
+  }
+]);
